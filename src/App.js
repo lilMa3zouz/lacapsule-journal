40bfa8260c4bb452f248e816d6f53d93c4570418
pc_home <m.mazouz67@gmail.com>
2020-11-25 09:01:55 +0100

    same

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import topBar from './img/bar.svg';
 import banner from './img/capsule2.png';
-import titre from './img/titreHeader.svg'
-import angleRight from './img/angle-right.svg'
-import angleLeft from './img/angle-left.svg'
+import titre from './img/titreHeader.svg'// eslint-disable-next-line
+import angleRight from './img/angle-right.svg'// eslint-disable-next-line
+import angleLeft from './img/angle-left.svg'// eslint-disable-next-line
 import motCroisé from './publication/motcroisé.jpg'
 import './css/bootstrap.min.css';
 import './css/materialIcons.css';
@@ -11,6 +11,10 @@ import './css/index.css';
 import './css/laptop.css';
 import './script.js'
 import * as $ from 'jquery';
+//import './css/allv5.1.0.css'
+//import './css/allv5.1.1.css'
+//import './css/allv5.3.0.css'
+
 require('bootstrap')
 
 function scrollFun(){
@@ -32,6 +36,12 @@ function scrollArrow(){
   $('#page').animate({scrollTop: getOffset}, 500);
 }
 
+function switchIcon(){
+  var trigger = document.getElementById("toggle");
+  var value = (trigger.innerHTML === "keyboard_arrow_right") ? "keyboard_arrow_down" : "keyboard_arrow_right"
+  trigger.innerHTML = value
+
+}
 function header(){
   return(
     <div id="header">
@@ -65,10 +75,49 @@ function header(){
 
 function lastCarousel() {
   return(
+  <div class="panel-group container" id="lastCarousel">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+          <a onClick={switchIcon} data-toggle="collapse" id="toggle" class="material-icons" data-parent="#lastCarousel" href="#collapse3">keyboard_arrow_right</a>
+      </div>
+      <div id="collapse3" class="panel-collapse collapse">
+        <div class="panel-body">
+        <div id="carouselExampleControls" className="carousel slide container" data-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <h1>n°1 - titre du numéro</h1>
+            <hr></hr>
+            <p>
+              <img className="cover" src="https://zupimages.net/up/20/47/ziw3.png"  alt="First slide" />
+            </p>
+          </div>
+          <div className="carousel-item">
+          <h1>n°2 - titre du numéro</h1>       
+          <hr></hr>
 
-    <div id="lastCarousel">
-      <button id="carouselTrigger" class="material-icons" >keyboard_arrow_right</button>
+          <p>
+            <img className="cover" src="https://zupimages.net/up/20/47/vx8b.png" alt="Second slide" />
+          </p>
+          </div>
+          <div className="carousel-item">
+          <h1>n°3 - titre du numéro</h1>
+          <hr></hr>
+          <p>
+            <img className="cover" src="https://zupimages.net/up/20/47/gia5.png" alt="Third slide" />
+          </p>
+          </div>
+        </div>
+        <a id="prev" className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <img src={angleLeft} className="controls" alt="prev"/>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <img src={angleRight} className="controls" alt="Next"/>
+        </a>
+</div>
+        </div>
+      </div>
     </div>
+  </div> 
   )
 }
 
